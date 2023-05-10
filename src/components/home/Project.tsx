7ed6462e56137c54c6f14b49278ab3f58b104cad
KingMaxud <maxud754@gmail.com>
2023-05-10 12:32:02 +0300

    added base projects animation

--- src/components/home/Project.tsx
+++ src/components/home/Project.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { motion, useScroll, useTransform } from 'framer-motion'
+
 import type { Project as ProjectType } from '../../types/project'
 
 type Props = {
@@ -6,19 +8,47 @@ type Props = {
 }
 
 const Project = ({ project }: Props) => {
+   const ref = useRef(null)
+
+   const { scrollYProgress } = useScroll({
+      target: ref,
+      offset: ['start end', 'end start'],
+   })
+
+   const translateDecorator = useTransform(
+      scrollYProgress,
+      [0, 1],
+      ['-20%', '20%'],
+   )
+
+   const translateDescription = useTransform(
+      scrollYProgress,
+      [0, 1],
+      ['30%', '-30%'],
+   )
+
    return (
-      <div className="flex relative max-w-4xl h-[15.75rem] mx-auto my-16">
+      <div
+         ref={ref}
+         className="flex relative max-w-4xl h-[15.75rem] mx-auto my-16"
+      >
          <div className="absolute w-[448px] h-[252px]">
             <img
                className="absolute z-10"
                src={project.images[0].url}
                alt={project.name.toLowerCase()}
             />
-            <div className="w-full h-full">
+            <motion.div
+               className="w-full h-full"
+               style={{ y: translateDecorator }}
+            >
                <div className="absolute top-0 left-6 w-full h-full border border-turquoise"></div>
-            </div>
+            </motion.div>
          </div>
-         <div className="bg-pinkish-beige max-w-2xl absolute w-[34.5rem] right-0 h-48 flex flex-col justify-between z-10">
+         <motion.div
+            style={{ translateY: translateDescription }}
+            className="bg-pinkish-beige max-w-2xl absolute w-[34.5rem] right-0 h-48 flex flex-col justify-between z-10"
+         >
             <h3 className="absolute bg-turquoise text-white text-2xl p-1.5 -top-11 left-6 font-mono">
                {project.name}
             </h3>
@@ -34,7 +64,7 @@ const Project = ({ project }: Props) => {
                   <a href={project.githubLink}>Github</a>
                </li>
             </ul>
-         </div>
+         </motion.div>
       </div>
    )
 }
